076d6b7a09f2e271a1182e0e69eed58a66b60a92
JOHNSON ADEOYE <johnsonadeoye888@gmail.com>
2023-10-02 15:42:51 -0700

    This is a 0x00. Python - Hello, World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -16,36 +16,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * print_listint - Print all elements of a listint_t list
- * @h: Pointer to the head of the list
- *
- * Return: Number of nodes in the list
- */
 size_t print_listint(const listint_t *h);
 
-/**
- * add_nodeint - Add a new node at the beginning of a listint_t list
- * @head: Pointer to the address of the head node
- * @n: Integer to be stored in the new node
- *
- * Return: Address of the new element or NULL if it fails
- */
 listint_t *add_nodeint(listint_t **head, const int n);
 
-/**
- * free_listint - Free a listint_t list
- * @head: Pointer to the head of the list
- */
 void free_listint(listint_t *head);
 
-/**
- * check_cycle - Check if a loop exists in a linked list (Floyd's Tortoise and Hare)
- * @list: Pointer to the head of the list
- *
- * Return: 1 if a loop exists, 0 otherwise
- */
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
