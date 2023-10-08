14b7ebd4e90f052b3f7199c59b693eaa57998ac2
JOHNSON ADEOYE <johnsonadeoye888@gmail.com>
2023-10-08 03:02:09 -0700

    This is a 0x02. Python - import & modules project task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+    """Python bytecode provided by ALX."""
     from magic_calculation_102 import add, sub
 
     if a < b:
