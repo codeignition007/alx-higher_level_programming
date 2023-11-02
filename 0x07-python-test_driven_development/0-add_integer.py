f96b580a7f1446a11097f3239744f77e6bfb06c0
JOHNSON ADEOYE <johnsonadeoye888@gmail.com>
2023-11-02 14:17:51 -0700

    Update test driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Defines an integer or float  addition function."""
+# 0-add_integer.py
+"""Defines an integer addition function."""
 
- 
 def add_integer(a, b=98):
-    """Returns a + b where a and b are integers
+    """
+    Adds two integers.
 
-    Float arguments are typecasted to ints before addition is performed.
+    Args:
+        a (int): The first integer.
+        b (int, optional): The second integer. Defaults to 98.
+
+    Returns:
+        int: The addition of a and b.
 
     Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+        TypeError: If a or b is not an integer or float.
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
