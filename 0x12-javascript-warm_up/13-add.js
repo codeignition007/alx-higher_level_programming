03e578f65ceaf27e3df5640f97c8e21cfd0e5f37
Johnson Adeoye <johnsonadeoye888@gmail.com>
2024-06-19 18:18:50 -0700

    W

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,13 +1,2 @@
 #!/usr/bin/node
-// add.js
-function add(a, b) {
-  return a + b;
-}
-
-const add = require('./add');
-
-console.log(add(3, 4)); // Output: 7
-console.log(add(10, 20)); // Output: 30
-
-module.exports = add;
-
+exports.add = (a, b) => a + b;
