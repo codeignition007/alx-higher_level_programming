6e241f2775f5f8898867e3b45a3211163b4fd838
Johnson Adeoye <johnsonadeoye888@gmail.com>
2024-06-19 18:11:09 -0700

    W

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const firstArg = args[0];
-
-if (firstArg === undefined) {
-  console.log("No argument");
-} else {
-  console.log(firstArg);
-}
-
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
